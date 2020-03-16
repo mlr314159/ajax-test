12b885587f337363bf2c677d38a704199e25809e
Mike <mlr314159@yahoo.co.uk>
2020-03-16 12:55:56 +0000

    going through json

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var xhr = new XMLHttpRequest();
+var data;
 
 xhr.open("GET", "https://swapi.co/api/");
 xhr.send();
@@ -6,5 +7,11 @@ xhr.send();
 xhr.onreadystatechange = function() {
     if(this.readyState == 4 && this.status == 200) {
         document.getElementById("data").innerHTML = this.responseText;
+        console.log(typeof(this.responseText));
+        console.log(typeof(JSON.parse(this.responseText)));
+        console.log(JSON.parse(this.responseText));
+        data = this.responseText;
     }
 };
+
+console.log(data);
