f9b7147374d3af86b74053b6de3fe8f05be6b7d0
Mike <mlr314159@yahoo.co.uk>
2020-03-23 10:09:20 +0000

    change

--- main.js
+++ main.js
@@ -4,14 +4,10 @@ var data;
 xhr.open("GET", "https://swapi.co/api/");
 xhr.send();
 
-function setData(jsonData){
-    data = jsonData;
-}
-
 xhr.onreadystatechange = function() {
     console.log(this.readyState);
     if(this.readyState == 4 && this.status == 200) {
-        setData(JSON.parse(this.responseText));
+        data=JSON.parse(this.responseText);
     }
 };
 
